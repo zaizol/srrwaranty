3edf3ffe4a55fffe90f5f4c9cb3df9fa33ce9c2b
unknown <srrwheelApp@gmail.com>
2019-07-15 21:34:52 +0700

    Update

--- routes/checkwaranty.js
+++ routes/checkwaranty.js
@@ -16,30 +16,49 @@ router.get('/', function(req, res, next) {
 });
 router.post('/getcustomer', function(req, res, next) {
     var reports = [];
-    Car.find({ 'carInfo.licenseNo': req.body.searchtext }).populate('customerID').exec(function(err, carcustomers) {
-        Customer.find({'customerInfo.mobile': { $regex: req.query.searchmobile, $options: 'i' } }).where('_id').in(carcustomers).exec(function(err, customers) {
+    var listcustomers = [];
+    if (req.body.search != '')
+    {
+        if (req.body.searchmobile != "")
+        {
+            Customer.find({'customerInfo.mobile': { $regex: req.body.searchmobile, $options: 'i' } }).exec(function(err, customers) {
             customers.forEach(function(x) { if (!listcustomers.includes(x._id)) listcustomers.push(x._id); });
-            Car.find({}).where('customerID').in(listcustomers).exec(function(err, cars) {
-                for (var i in customers) {
-                    customer = customers[i];
-                    var licenNO = "";
-                    for (var t in cars) {
-                        car = cars[t];
-                        if (car.customerID.equals(customer._id)) {
-                            licenNO = licenNO + car.carInfo.licenseNo + ",";
-                        }
-                    }
-                    reports.push({ id: customer._id, fullname: customer.customerInfo.name + " " + customer.customerInfo.lastname, mobile: customer.customerInfo.mobile, licenseNo: licenNO, lastservice: customer.LastServicedate });
+            Car.find({ 'carInfo.licenseNo': req.body.search }).where('customerID').in(listcustomers).populate('customerID').exec(function(err, carcustomers) {
+                for (var i in carcustomers) {
+                    customer = carcustomers[i];
+                    reports.push({ id: customer.customerID._id, fullname: customer.customerID.customerInfo.name + " " + customer.customerID.customerInfo.lastname, mobile: customer.customerID.customerInfo.mobile });
                 }
                 res.end(JSON.stringify(reports));
             });
         });
-        /*for (var i in carcustomers) {
-            customer = carcustomers[i];
-            reports.push({ id: customer.customerID._id, fullname: customer.customerID.customerInfo.name + " " + customer.customerID.customerInfo.lastname, mobile: customer.customerID.customerInfo.mobile });
+        }else{
+            Car.find({ 'carInfo.licenseNo': req.body.search }).populate('customerID').exec(function(err, carcustomers) {
+                for (var i in carcustomers) {
+                    customer = carcustomers[i];
+                    reports.push({ id: customer.customerID._id, fullname: customer.customerID.customerInfo.name + " " + customer.customerID.customerInfo.lastname, mobile: customer.customerID.customerInfo.mobile });
+                }
+                res.end(JSON.stringify(reports));
+            });
+        }   
+    }else{
+        if (req.body.searchmobile != "")
+        {
+            console.log(req.body.searchmobile);
+            Customer.find({'customerInfo.mobile': { $regex: req.body.searchmobile, $options: 'i' } }).exec(function(err, customers) {
+                customers.forEach(function(x) { if (!listcustomers.includes(x._id)) listcustomers.push(x._id); });
+                Car.find({}).where('customerID').in(listcustomers).populate('customerID').exec(function(err, carcustomers) {
+                    for (var i in carcustomers) {
+                        customer = carcustomers[i];
+                        reports.push({ id: customer.customerID._id, fullname: customer.customerID.customerInfo.name + " " + customer.customerID.customerInfo.lastname, mobile: customer.customerID.customerInfo.mobile });
+                    }
+                    res.end(JSON.stringify(reports));
+                });
+            });
+        }else{
+            res.end(JSON.stringify(reports));
         }
-        res.end(JSON.stringify(reports));*/
-    });
+    }
+   
 });
 
 router.post('/getcar', function(req, res, next) {
